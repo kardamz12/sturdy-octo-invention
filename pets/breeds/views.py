e03075e686d00f9c513efce7cd0f64cc2bb0ef0f
unknown <kardamz@gmail.com>
2020-03-27 07:58:24 +0530

    Added try except block in functions in views.py

--- pets/breeds/views.py
+++ pets/breeds/views.py
@@ -6,39 +6,56 @@ from .models import Breed
 # Create your views here.
 
 def get_breedlist(request):
-    queryset = Breed.objects.all()
-    return render(request, 'get_breedlist.html', {'profiles':queryset})
+    try:
+        queryset = Breed.objects.all()
+        return render(request, 'get_breedlist.html', {'profiles':queryset})
+    except queryset.DoesNotExist:
+        messages.info(request, "Couldn't fetch the Breed list.")
 
 def post_breedlist(request):
-    if request.method == 'POST':
-        name = request.POST['name']
-        size = request.POST['size']
-        friendliness = request.POST['friendliness']
-        trainability = request.POST['trainability']
-        sheddingamount = request.POST['sheddingamount']
-        exerciseneeds = request.POST['exerciseneeds']
+    try:
+        if request.method == 'POST':
+            name = request.POST['name']
+            size = request.POST['size']
+            friendliness = request.POST['friendliness']
+            trainability = request.POST['trainability']
+            sheddingamount = request.POST['sheddingamount']
+            exerciseneeds = request.POST['exerciseneeds']
 
-        user = User.objects.create_user(name=name, size=size, friendliness=friendliness, trainability=trainability, sheddingamount=sheddingamount,
-                                        exerciseneeds=exerciseneeds)
-        user.save();
-        messages.info(request, 'Breed details added.')
-        return redirect('get_breedlist')
-    else:
-        messages.info(request, 'Breed details not added.')
-        return render(request, 'get_breedlist')
+            user = User.objects.create_user(name=name, size=size, friendliness=friendliness, trainability=trainability, sheddingamount=sheddingamount,
+                                            exerciseneeds=exerciseneeds)
+            user.save();
+            messages.info(request, 'Breed details added. Please find the updated Breed list below:')
+            queryset = Breed.objects.all()
+            return render(request, 'get_breedlist.html', {'profiles': queryset})
+        else:
+            messages.info(request, 'Breed details not added.')
+            queryset = Breed.objects.all()
+            return render(request, 'get_breedlist.html', {'profiles': queryset})
+    except:
+        messages.info(request, 'Breed details not added(Inside except)')
 
 def get_breeddetail(request):
-    queryset = Breed.objects.get(id=2)
-    return render(request, 'get_breed.html', {'profile': queryset})
+    try:
+        queryset = Breed.objects.get(id=2)
+        return render(request, 'get_breed.html', {'profile': queryset})
+    except queryset.DoesNotExist:
+        messages.info(request, "Couldn't fetch the Breed details.")
 
 def put_breeddetail(request):
-    queryset = Breed.objects.get(id=1)
-    queryset.name = "abc"
-    queryset.save()
-    return render(request, 'get_breed.html', {'profile': queryset})
+    try:
+        queryset = Breed.objects.get(id=2)
+        queryset.name = "Jackie"
+        queryset.save()
+        return render(request, 'get_breed.html', {'profile': queryset})
+    except queryset.DoesNotExist:
+        messages.info(request, "Couldn't update a Breed details.")
 
 def delete_breeddetail(request):
-    queryset = Breed.objects.get(name='abc')
-    queryset.delete()
-    queryset = Breed.objects.all()
-    return render(request, 'get_breedlist.html', {'profiles': queryset})
+    try:
+        queryset = Breed.objects.get(name='abc')
+        queryset.delete()
+        queryset = Breed.objects.all()
+        return render(request, 'get_breedlist.html', {'profiles': queryset})
+    except queryset.DoesNotExist:
+        messages.info(request, "Couldn't delete a Breed details.")
--- pets/dogs/views.py
+++ pets/dogs/views.py
@@ -5,43 +5,63 @@ from .models import Dog
 
 # Create your views here.
 def base(request):
-    return render(request, 'base.html')
+    try:
+        return render(request, 'base.html')
+    except:
+        messages.info(request, "Couldn't render 'base.html'.")
 
 def get_doglist(request):
-    queryset = Dog.objects.all()
-    return render(request, 'get_doglist.html', {'profiles':queryset})
+    try:
+        queryset = Dog.objects.all()
+        return render(request, 'get_doglist.html', {'profiles':queryset})
+    except queryset.DoesNotExist:
+        messages.info(request, "Couldn't fetch the Dog list.")
 
 def post_doglist(request):
-    if request.method == 'POST':
-        name = request.POST['name']
-        age = request.POST['age']
-        breed = request.POST['breed']
-        gender = request.POST['gender']
-        color = request.POST['color']
-        favoritefood = request.POST['favoritefood']
-        favoritetoy = request.POST['favoritetoy']
+    try:
+        if request.method == 'POST':
+            name = request.POST['name']
+            age = request.POST['age']
+            breed = request.POST['breed']
+            gender = request.POST['gender']
+            color = request.POST['color']
+            favoritefood = request.POST['favoritefood']
+            favoritetoy = request.POST['favoritetoy']
 
-        user = User.objects.create_user(name=name, age=age, breed=breed, gender=gender, color=color,
-                                        favoritefood=favoritefood, favoritetoy=favoritetoy)
-        user.save();
-        messages.info(request, 'Dog details added.')
-        return redirect('get_doglist')
-    else:
-        messages.info(request, 'Dog details not added.')
-        return render(request, 'get_doglist')
+            user = User.objects.create_user(name=name, age=age, breed=breed, gender=gender, color=color,
+                                            favoritefood=favoritefood, favoritetoy=favoritetoy)
+            user.save();
+            messages.info(request, 'Dog details added. Please find the updated Dog list below:')
+            queryset = Dog.objects.all()
+            return render(request, 'get_doglist.html', {'profiles': queryset})
+        else:
+            messages.info(request, 'Dog details not added.')
+            queryset = Dog.objects.all()
+            return render(request, 'get_doglist.html', {'profiles': queryset})
+    except:
+        messages.info(request, 'Dog details not added(Inside except)')
 
 def get_dogdetail(request):
-    queryset = Dog.objects.get(id=2)
-    return render(request, 'get_dog.html', {'profile': queryset})
+    try:
+        queryset = Dog.objects.get(id=2)
+        return render(request, 'get_dog.html', {'profile': queryset})
+    except queryset.DoesNotExist:
+        messages.info(request, "Couldn't fetch the Dog details.")
 
 def put_dogdetail(request):
-    queryset = Dog.objects.get(id=1)
-    queryset.name = "Neelu"
-    queryset.save()
-    return render(request, 'get_dog.html', {'profile': queryset})
+    try:
+        queryset = Dog.objects.get(id=1)
+        queryset.name = "Neel"
+        queryset.save()
+        return render(request, 'get_dog.html', {'profile': queryset})
+    except queryset.DoesNotExist:
+        messages.info(request, "Couldn't update a Dog details.")
 
 def delete_dogdetail(request):
-    queryset = Dog.objects.get(name='aa')
-    queryset.delete()
-    queryset = Dog.objects.all()
-    return render(request, 'get_doglist.html', {'profiles': queryset})
+    try:
+        queryset = Dog.objects.get(name='aa')
+        queryset.delete()
+        queryset = Dog.objects.all()
+        return render(request, 'get_doglist.html', {'profiles': queryset})
+    except queryset.DoesNotExist:
+        messages.info(request, "Couldn't delete a Dog details.")
